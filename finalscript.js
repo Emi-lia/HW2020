e5ae00077d10a4ba26afb34ba0bb6ab360f8b21f
emilia <3010026497@qq.com>
2020-06-17 12:51:10 +0800

    之前报错太多的版本推翻重做，添加了三个final文件

--- finalscript.js
+++ finalscript.js
@@ -1,97 +1,92 @@
+
 let tasks = [];
+
 
 function renderEditor() {
     let inputEl = document.querySelector("#default-todo-panel .todo-editor > input");
-
-
 
     let addTask = () => {
         if (inputEl.value.length == 0) {
             return;
         }
         let newTask = {
-
-            title: inputEl.Value,
+            title: inputEl.value,
             done: false,
+            important:false,
         };
-
-        inputEl.value = " ";
-
+        inputEl.value = "";
         tasks.push(newTask);
-
         console.log("tasks: ", tasks);
-
         renderTaskItems();
-
     };
-
     inputEl.onkeypress = (e) => {
         if (e.key === "Enter") {
             addTask();
         }
     };
-
     let addEl = document.querySelector("#default-todo-panel .todo-editor > button");
     addEl.onclick = (e) => {
-        console.log("add click");
+        addTask();
     };
 }
 
+
+
+
 function renderTaskItems() {
+    console.log("render items");
     let itemsEl = document.querySelector("#default-todo-panel .todo-items");
     itemsEl.querySelectorAll("div").forEach((node) => node.remove());
-
+    console.log(itemsEl);
     for (let i = 0; i < tasks.length; i++) {
         let task = tasks[i];
         let itemEl = document.createElement("div");
         itemEl.className = "task";
-
         let doneEl = document.createElement("input");
         doneEl.type = "checkbox";
         doneEl.checked = task.done;
         if (task.done) {
-            itemsEl.classList.add("done");
+            itemEl.classList.add("done");
         } else {
             itemEl.classList.remove("done");
         }
-
         doneEl.onchange = (e) => {
             task.done = e.target.checked;
+            //console.log("checkbox",e);
             if (task.done) {
-                itemsEl.classList.add("done");
+                itemEl.classList.add("done");
             } else {
                 itemEl.classList.remove("done");
             }
         }
         itemEl.append(doneEl);
-
         let titleEl = document.createElement("label");
         titleEl.innerText = task.title;
         itemEl.append(titleEl);
-
-       let ctrlbarEl = renderTaskCtrlBar(tasks,i);
-
+        let ctrlbarEl = renderTaskCtrlBar(tasks, i);
         itemEl.append(ctrlbarEl);
         itemsEl.append(itemEl);
     }
 }
-function renderTaskCtrlBar(tasks,taskIdx){
+
+
+
+function renderTaskCtrlBar(tasks, taskIdx) {
     let ctrlbarEl = document.createElement("div");
     ctrlbarEl.className = "ctrlbar";
-
     let upEl = document.createElement("button");
-    if (taskIdx ===0 ){
+    if (taskIdx ===0){
         upEl.disabled = true;
     }
     upEl.innerText = "↑";
-    upEl.onclick = ()=>{
+    upEl.onclick = () => {
 
     };
     ctrlbarEl.append(upEl);
 
     let downEl = document.createElement("button");
     downEl.innerText = "↓";
-    downEl.onclick = ()=>{
+    downEl.onclick = () => {
 
     };
     ctrlbarEl.append(downEl);
@@ -99,11 +94,13 @@ function renderTaskCtrlBar(tasks,taskIdx){
     let cancelEl = document.createElement("button");
     cancelEl.innerText = "X";
     cancelEl.onclick = () => {
-        tasks.splice(i, 1);
+        tasks.splice(taskIdx, 1);
         renderTaskItems();
     };
 
     ctrlbarEl.append(cancelEl);
+
+    return ctrlbarEl;
 }
 
 renderEditor();
