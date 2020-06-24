adbcffa650e42e5424817f5f205bff197ae5b2c8
emilia <3010026497@qq.com>
2020-06-24 20:13:26 +0800

    修改了背景图片，添加了完成截止日期

--- finalscript.js
+++ finalscript.js
@@ -1,29 +1,33 @@
 
-let tasks = [];
+let tasks = [];//创建一个tasks数组
 
 
+
+//本函数提供输入、按键提交、enter提交功能
 function renderEditor() {
     let inputEl = document.querySelector("#default-todo-panel .todo-editor > input");
-
+    let deadlineEl = document.querySelector("#default-todo-panel .todo-editor > input#deadline");
+    inputEl.onkeypress = (e) => {
+        if (e.key === "Enter") {
+            addTask();
+        }
+    };
     let addTask = () => {
         if (inputEl.value.length == 0) {
             return;
         }
         let newTask = {
             title: inputEl.value,
+            dl: deadlineEl.value,
             done: false,
             import: false,
         };
         inputEl.value = "";
+        deadlineEl.value = "";
         tasks.push(newTask);
-        console.log("tasks: ", tasks);
         renderTaskItems();
     };
-    inputEl.onkeypress = (e) => {
-        if (e.key === "Enter") {
-            addTask();
-        }
-    };
+
     let addEl = document.querySelector("#default-todo-panel .todo-editor > button");
     addEl.onclick = (e) => {
         addTask();
@@ -31,18 +35,16 @@ function renderEditor() {
 }
 
 
-
-
+//本函数实现了完成、title、重要程度
 function renderTaskItems() {
-    console.log("render items");
     let itemsEl = document.querySelector("#default-todo-panel .todo-items");
-    itemsEl.querySelectorAll("div").forEach((node) => node.remove());
-    console.log(itemsEl);
+    itemsEl.querySelectorAll("div").forEach((node) => node.remove());//删除之前已经添加的项目
+    //创建新元素
     for (let i = 0; i < tasks.length; i++) {
         let task = tasks[i];
         let itemEl = document.createElement("div");
         itemEl.className = "task";
-        //事项完成
+        //事项完成元素
         let doneEl = document.createElement("input");
         doneEl.type = "checkbox";
         doneEl.checked = task.done;
@@ -64,10 +66,16 @@ function renderTaskItems() {
         let titleEl = document.createElement("label");
         titleEl.innerText = task.title;
         itemEl.append(titleEl);
+        //获取完成日期
+        let dlEl = document.createElement("label");
+        dlEl.id = "dl";
+        dlEl.innerHTML = task.dl;
+        itemEl.append(dlEl);
+        //创建操作栏元素
         let ctrlbarEl = renderTaskCtrlBar(tasks, i);
         itemEl.append(ctrlbarEl);
         itemsEl.append(itemEl);
-        //重要程度
+        //创建重要程度元素
         let impEl = document.querySelectorAll(".ctrlbar input")[i];
         impEl.checked = task.import;
         if (task.import) {
@@ -89,7 +97,7 @@ function renderTaskItems() {
 }
 
 
-
+//本函数实现操作栏里边的上移、下移、重要程度、删除
 function renderTaskCtrlBar(tasks, taskIdx) {
     let ctrlbarEl = document.createElement("div");
     ctrlbarEl.className = "ctrlbar";
@@ -99,24 +107,27 @@ function renderTaskCtrlBar(tasks, taskIdx) {
     ctrlbarEl.append(impEl);
     //上移按钮
     let upEl = document.createElement("button");
-    if (taskIdx ===0){
+    if (taskIdx === 0) {
         upEl.disabled = true;
     }
     upEl.innerText = "↑";
     upEl.onclick = () => {
-        let t=tasks[taskIdx];
-        tasks[taskIdx]=tasks[taskIdx-1];
-        tasks[taskIdx-1]=t;
+        let t = tasks[taskIdx];
+        tasks[taskIdx] = tasks[taskIdx - 1];
+        tasks[taskIdx - 1] = t;
         renderTaskItems();
     };
     ctrlbarEl.append(upEl);
     //下移按钮
     let downEl = document.createElement("button");
+    if (taskIdx+1 === tasks.length){
+        downEl.disabled = true;
+    }
     downEl.innerText = "↓";
     downEl.onclick = () => {
-        let t=tasks[taskIdx];
-        tasks[taskIdx]=tasks[taskIdx+1];
-        tasks[taskIdx+1]=t;
+        let t = tasks[taskIdx];
+        tasks[taskIdx] = tasks[taskIdx + 1];
+        tasks[taskIdx + 1] = t;
         renderTaskItems();
     };
     ctrlbarEl.append(downEl);
@@ -127,6 +138,8 @@ function renderTaskCtrlBar(tasks, taskIdx) {
         tasks.splice(taskIdx, 1);
         renderTaskItems();
     };
+
+
 
     ctrlbarEl.append(cancelEl);
 
